8d8a2693dfbca21724da1f6220adedd58fce03cb
susumu0729 <134839930+susumu0729@users.noreply.github.com>
2023-10-29 13:37:35 +0900

    kadai_011.py upload

---
+++ kadai_011.py
@@ -0,0 +1,13 @@
+list1 = ["水","金","地","火","木","土","天","海","冥"]
+
+for l in list1:
+    print(l)
+
+
+count = 0
+
+while count < 9:
+    for l in list1:
+        print(l)
+        count = count + 1
+
